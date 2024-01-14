0fd2e36badb4370986740c365cdb37814e3e8740
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-14 17:06:46 +0530

    feat: add dynamic rendering

--- src/save.js
+++ src/save.js
@@ -1,24 +1,8 @@
 /**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
+ * See https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-edit-save/#save
  *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ * @return {null} Dynamic blocks do not save the HTML.
  */
-import { useBlockProps } from '@wordpress/block-editor';
+const Save = () => null;
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {Element} Element to render.
- */
-export default function save() {
-	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Sg Typing Text Block - hello from the saved content!' }
-		</p>
-	);
-}
+export default Save;
