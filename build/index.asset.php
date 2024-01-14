080264a350f0b7f084dc542b9a6421fc5b8de0d6
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-14 17:07:52 +0530

    feat: generate build for dynamic rendering

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '192010fdcbcbde85c738');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8f3d811c0293cb0d7d35');
