97db226a0b677e7b73f82a13d36737da615faac4
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-14 22:58:17 +0530

    feat: use sass variable instead of css variable

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4cf539dfc3ee9abd75df');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1e4d927e10f14cbc3d2f');
